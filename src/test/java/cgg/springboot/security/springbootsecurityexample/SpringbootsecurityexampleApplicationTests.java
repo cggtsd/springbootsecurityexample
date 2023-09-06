b191c9b055efacceafe674b4944eac5ed3a2fd7c
cggnewtsds <cggnewtsds@gmail.com>
2023-09-06 17:31:48 +0530

    commit 06-09-2023

---
+++ src/test/java/cgg/springboot/security/springbootsecurityexample/SpringbootsecurityexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package cgg.springboot.security.springbootsecurityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsecurityexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
